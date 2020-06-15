6f9430d4da6cb3c7e99d008a063d95ad30602767
su <sebastian.ullrich@kontextwork.de>
2020-06-15 14:45:16 +0200

    Inital commit

---
+++ src/main/java/de/kontextwork/virginspring/VirginSpringApplication.java
@@ -0,0 +1,13 @@
+package de.kontextwork.virginspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirginSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirginSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/kontextwork/virginspring/VirginSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package de.kontextwork.virginspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirginSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
