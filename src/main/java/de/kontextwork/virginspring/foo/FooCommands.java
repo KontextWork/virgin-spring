41174c3533da1bec32c8d8161970f6ce97d7a0f8
su <sebastian.ullrich@kontextwork.de>
2020-06-15 15:25:05 +0200

    Add spring shell

---
+++ src/main/java/de/kontextwork/virginspring/foo/FooCommands.java
@@ -0,0 +1,15 @@
+package de.kontextwork.virginspring.foo;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.shell.standard.*;
+
+@Log4j2
+@ShellComponent
+public class FooCommands
+{
+  @ShellMethod("Greets you")
+  public String hello(@ShellOption String name)
+  {
+    return String.format("Hello %s!", name);
+  }
+}
